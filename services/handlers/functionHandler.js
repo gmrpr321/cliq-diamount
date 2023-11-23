b34f6c749dba4fd30373018ca9ac8b4f318cb1a0
pradeep sn <pradeepsn606@gmail.com>
2023-11-23 15:53:25 +0530

    fin

--- services/handlers/functionHandler.js
+++ services/handlers/functionHandler.js
@@ -43,13 +43,12 @@ const Functions = (function () {
           text: "Diagram Generation in-progress, Please click the button after a few seconds.\nPlease prompt again if you don't get the Diagram within 2 minutes",
         };
       return {
-        title: matchingEntry.prompt,
         text: `Image URL : ${matchingEntry.shortUrl}`,
         card: { theme: "modern-inline" },
         slides: [
           {
             type: "images",
-            title: "",
+            title: `${matchingEntry.prompt}`,
             data: [matchingEntry.shortUrl],
           },
         ],

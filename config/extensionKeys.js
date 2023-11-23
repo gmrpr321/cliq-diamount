bf4a06bd6e4b81ecff76704e83ba0757e2e1e7a3
pradeep sn <pradeepsn606@gmail.com>
2023-11-23 14:57:00 +0530

    fin

--- config/extensionKeys.js
+++ config/extensionKeys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  baseUrl: "https://real-rose-swallow.cyclic.app/",
+  baseUrl: "https://real-rose-swallow.cyclic.app",
   openaiKey: "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",
 };

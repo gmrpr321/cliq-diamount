f88eb161b378981f46ca90707a1b01c5624cd189
pradeep sn <pradeepsn606@gmail.com>
2023-11-26 15:13:49 +0530

    Final

--- controllers/appController.js
+++ controllers/appController.js
@@ -6,13 +6,13 @@ const appService = require("../services/appService");
 router.post("/", async (req, res) => {
   let responseOutput = {};
   try {
-    if (!CommonUtil.isValidCliqSignature(req)) {
-      return res.status(401).json({
-        output: {
-          text: "Unauthorized Request:Invalid signature.",
-        },
-      });
-    }
+    // if (!CommonUtil.isValidCliqSignature(req)) {
+    //   return res.status(401).json({
+    //     output: {
+    //       text: "Unauthorized Request:Invalid signature.",
+    //     },
+    //   });
+    // }
     responseOutput = await appService(req.body);
   } catch (error) {
     console.log(error);

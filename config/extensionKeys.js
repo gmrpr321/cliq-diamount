7bb07873c1f5af71df2266c6e23dbf99b83d1d6b
pradeep sn <pradeepsn606@gmail.com>
2023-12-02 04:13:40 +0530

    temp

--- config/extensionKeys.js
+++ config/extensionKeys.js
@@ -1,4 +1,4 @@
 module.exports = {
   baseUrl: "https://navy-blue-indri-veil.cyclic.app",
-  openaiKey: "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",
+  openaiKey: "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",
 };
